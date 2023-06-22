2892a9e9d493ef05419ab89b9f2ed1cdd181e21e
ArinaOwl <67727777+ArinaOwl@users.noreply.github.com>
2023-06-23 00:02:52 +0300

    Update utils.py

--- emotion_bot/emotion_bot/utils.py
+++ emotion_bot/emotion_bot/utils.py
@@ -15,7 +15,7 @@ class EmotionRecognition:
         self.model = ASTForAudioClassification.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593",
                                                                num_labels=4, return_dict=False,
                                                                ignore_mismatched_sizes=True)
-        self.model.load_state_dict(torch.load('ast_model_weights.pth'))
+        #self.model.load_state_dict(torch.load('ast_model_weights.pth'))
         self.model.eval()
         self.to_probs = torch.nn.Softmax(dim=0).requires_grad_(False)
 
@@ -30,7 +30,6 @@ class EmotionRecognition:
             probs (np.ndarray) - вероятности распознавания для каждой эмоции."""
         features = self.feature_extractor(waveform, sampling_rate, return_tensors='pt')
         outputs, = self.model(features.input_values.to(torch.float32))
-        print(outputs)
         probs = self.to_probs(outputs[0])
         return np.round(probs.detach().numpy() * 100).astype(int)
 
@@ -48,5 +47,4 @@ def check_voiced(waveform, sampling_rate):
     f0, voicing, voicing_p = librosa.pyin(waveform, sr=sampling_rate,
                                           fmin=64., fmax=1046.,
                                           frame_length=512, fill_na=0.)
-    print(f0)
     return np.max(f0) > 0
